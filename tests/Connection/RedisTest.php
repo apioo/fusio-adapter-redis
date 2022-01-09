7b7d03fc0b685e925ba5162bd1892134ca7f2f8c
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-01-09 14:48:56 +0100

    fix test

--- tests/Connection/RedisTest.php
+++ tests/Connection/RedisTest.php
@@ -66,7 +66,7 @@ class RedisTest extends TestCase
 
         $this->assertInstanceOf(Container::class, $builder->getForm());
 
-        $elements = $builder->getForm()->getProperty('element');
+        $elements = $builder->getForm()->getElements();
         $this->assertEquals(2, count($elements));
         $this->assertInstanceOf(Input::class, $elements[0]);
         $this->assertInstanceOf(Input::class, $elements[1]);
